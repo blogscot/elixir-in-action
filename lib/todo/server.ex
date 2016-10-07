4560e4b1b86e50edf4a00c80cd370d2d9f0e3934
blogscot <iain.diamond@gmail.com>
2016-10-07 01:25:30 +0100

    Add basic persistence

--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -1,26 +1,28 @@
 defmodule Todo.Server do
   use GenServer
 
-  def init(_) do
-    {:ok, Todo.List.new}
+  def init(name) do
+    {:ok, {name, Todo.Database.get(name) || Todo.List.new}}
   end
 
-  def handle_cast({:add_entry, new_entry}, todo_list) do
-    {:noreply, Todo.List.add_entry(todo_list, new_entry)}
+  def handle_cast({:add_entry, new_entry}, {name, todo_list}) do
+    new_state = Todo.List.add_entry(todo_list, new_entry)
+    Todo.Database.store(name, new_state)
+    {:noreply, {name, new_state}}
   end
 
-  def handle_cast({:delete, entry_id}, todo_list) do
+  def handle_cast({:delete, entry_id}, {_name, todo_list}) do
     {:noreply, Todo.List.delete_entry(todo_list, entry_id)}
   end
 
-  def handle_call({:entries, date}, _from, todo_list) do
-    {:reply, Todo.List.entries(todo_list, date), todo_list}
+  def handle_call({:entries, date}, _from, {name, todo_list}) do
+    {:reply, Todo.List.entries(todo_list, date), {name, todo_list}}
   end
 
   # Client API
 
-  def start do
-    GenServer.start(Todo.Server, nil)
+  def start(list_name) do
+    GenServer.start(Todo.Server, list_name)
   end
   def add_entry(pid, %{date: _date, title: _title}=new_entry) do
     GenServer.cast(pid, {:add_entry, new_entry})
